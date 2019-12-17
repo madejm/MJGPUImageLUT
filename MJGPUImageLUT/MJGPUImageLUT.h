a5a0507ee3a4984189a08b76f0a46cbd743ce7ff
Mateusz Madej <madejmt@gmail.com>
2019-12-18 00:58:38 +0100

    Initial Commit

---
+++ MJGPUImageLUT/MJGPUImageLUT.h
@@ -0,0 +1,19 @@
+//
+//  MJGPUImageLUT.h
+//  MJGPUImageLUT
+//
+//  Created by Mateusz Madej on 18/12/2019.
+//  Copyright © 2019 Mateusz Madej. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MJGPUImageLUT.
+FOUNDATION_EXPORT double MJGPUImageLUTVersionNumber;
+
+//! Project version string for MJGPUImageLUT.
+FOUNDATION_EXPORT const unsigned char MJGPUImageLUTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJGPUImageLUT/PublicHeader.h>
+
+
